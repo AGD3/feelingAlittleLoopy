38349681f15bf868bec4bae9cead12fffb2e1e33
AGD3 <49073762+AGD3@users.noreply.github.com>
2020-03-18 18:02:39 -0700

    All 5 questions answered

---
+++ JS/scripts.js
@@ -0,0 +1,45 @@
+// // Loop 1
+
+// let i = 0;
+// while(i < 3) {
+//     console.log("I'm not starting to feel a little loopy");
+//     i++;
+// }
+
+
+// // Loop 2
+// let i = 0;
+// while(i <= 20) {
+//     if (i % 2 === 0)
+//     console.log(` ${i} ${" is even."} `);
+//     else if (i % 2 != 0);
+//     console.log(` ${i} ${" is odd."} `);
+//     i++;
+
+// }
+
+// Loop 3
+
+// let i = 0; 
+// while (i <= 10) {
+//     result = i * 9;
+//     console.log(` ${i} * 9 = ${result} `);
+//     i++;
+// }
+
+// // Loop 4
+// let i = 36;
+// while(i > 1) {
+//     console.log(i);
+//     i = i / 2;
+
+// }
+
+
+// Loop 5
+
+let i = 0;
+while ( i <= 80 ) {
+        console.log (` There is only room for ${80 - i} more books! `);
+        i++;
+}
